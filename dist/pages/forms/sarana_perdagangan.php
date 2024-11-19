946f1d47ad286357856d5a9cb1ad36c2f89b4855
Sabrina Tria Amanda <sabrina.ta03@gmail.com>
2024-11-19 15:05:46 +0700

    membuat lembaga keuangan

--- dist/pages/forms/sarana_perdagangan.php
+++ dist/pages/forms/sarana_perdagangan.php
@@ -357,7 +357,7 @@ include "../../config/session.php";
                                             <input type="number" class="form-control" placeholder="Isi angka/jumlah" min="0" step="1" style="width: 100%;">
                                         </div> 
                                     </div>
-                                     
+                                    
                                     <!-- /.col -->
                                     <!-- /.col -->
                                     <div class>
