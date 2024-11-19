33a93ab28b54bb1a7fb0ead04a6af6babae501fb
Bayu Perasetio <bayuperasetio00@gmail.com>
2024-11-19 11:53:23 +0700

    menghapus desa

--- dist/pages/forms/pendudukan.php
+++ dist/pages/forms/pendudukan.php
@@ -241,7 +241,7 @@ include "../../config/session.php";
 
                     <div class="card card-primary card-outline mb-4">
                         <div class="card-header mb-3">
-                            <h3 class="card-title">Penduduk</h3>
+                            <h3 class="card-title">Penduduk</h3> 
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool batas-wilayah">
                                     <i class="fas fa-minus"></i>
