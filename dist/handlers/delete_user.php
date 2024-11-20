a24f2a934652115f9cc86edf533eb021088964a9
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2024-11-20 13:33:30 +0700

    Update agar bisa menghapus user yang sudah terkait dengan progress pengisian form

--- dist/handlers/delete_user.php
+++ dist/handlers/delete_user.php
@@ -4,6 +4,12 @@ include_once('../config/conn.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
 
+    $query = "DELETE FROM user_progress WHERE user_id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute(); // Hapus data yang tergantung
+
+    // Sekarang hapus user
     $query = "DELETE FROM users WHERE id = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("i", $id);
@@ -14,9 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         header("Location: ../pages/tables/user.php?messagedelete=error");
     }
 
+
     $stmt->close();
     $conn->close();
 } else {
     header("Location: ../pages/tables/user.php");
 }
-?>
