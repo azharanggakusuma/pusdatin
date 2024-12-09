d511004fe247402209868f23a9f09f38642d7c68
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2024-12-09 15:03:04 +0700

    Added new form names to keadaan_geografi.php

--- dist/pages/forms/keadaan_geografi.php
+++ dist/pages/forms/keadaan_geografi.php
@@ -17,7 +17,21 @@ $user_id = $user['id'] ?? 0;
 $forms = [
     'Luas Wilayah Desa',
     'Batas Wilayah Desa',
-    // Add other form names here
+    'Jarak Kantor Desa',
+    'Titik Koordinat Kantor Desa',
+    'Topografi Terluas Wilayah Desa',
+    'Luas Tanah Kas Desa',
+    'Status Pemerintahan Desa dan Klasifikasi Berdasarkan Tingkat Perkembangannya',
+    'Banyaknya Dusun, Rukun Tetangga dan Rukun Warga',
+    'Alamat Balai Desa/Kantor Kelurahan',
+    'Dasar hukum pembentukan Pemerintah Desa / Kelurahan',
+    'Dasar hukum pembentukan Badan Permusyawaratan Desa (BPD)',
+    'Ketersediaan Penetapan Batas dan Peta Desa',
+    'Alamat Website dan Media Sosial',
+    'Kepemilikan Kantor Kepala Desa/Balai Desa',
+    'Kondisi Kantor Kepala Desa/Balai Desa',
+    'Perkembangan Status Indeks Desa Membangun (IDM) di Kantor Desa',
+    'Ketersediaan Internet dan Komputer/PC/laptop di Kantor Desa',
 ];
 
 // Initialize an array to store form lock status
