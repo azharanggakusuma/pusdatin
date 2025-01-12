57ab2114e4fe4b754fe7ad244f2158087c66da1f
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-01-12 15:18:27 +0700

    Added conditional statement to display 'Periode Tahun' header in sidebar

--- dist/components/sidebar.php
+++ dist/components/sidebar.php
@@ -20,7 +20,9 @@ $current_page = basename($_SERVER['SCRIPT_NAME']);
     <div class="sidebar-wrapper">
         <nav class="mt-2">
             <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-                <li class="nav-header mb-2"><b><i>Periode Tahun (<?php echo htmlspecialchars($tahun); ?>)</i></b></li>
+                <?php if ($level == 'user'): ?>
+                    <li class="nav-header mb-2"><b><i>Periode Tahun (<?php echo htmlspecialchars($tahun); ?>)</i></b></li>
+                <?php endif; ?>
                 <li class="nav-item menu-open">
                     <a href="<?php echo $base_path; ?>index.php" class="nav-link <?php echo $current_page == 'index.php' ? 'active' : ''; ?>">
                         <i class="nav-icon bi bi-speedometer"></i>
