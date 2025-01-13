0c61c15eee2dda563e1b0f777240243a648120b2
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-01-13 11:01:58 +0700

    Update footer version from 1.0 to Beta

--- dist/components/footer.php
+++ dist/components/footer.php
@@ -1,5 +1,5 @@
 <footer class="app-footer"> <!--begin::To the end-->
-  <div class="float-end d-none d-sm-inline">Version 1.0</div> <!--end::To the end--> <!--begin::Copyright-->
+  <div class="float-end d-none d-sm-inline">Version Beta</div> <!--end::To the end--> <!--begin::Copyright-->
   <strong>
     Copyright &copy; <?php echo date('Y'); ?> &nbsp;
     <a href="#" class="text-decoration-none">Diskominfo Kab. Cirebon</a>.
