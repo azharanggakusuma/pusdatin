635e8525033195cae2f8520ecd588f702b8a7509
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-01-17 10:51:22 +0700

    Update form action in perlindungan_sosial,_pembangunan,_dan_pemberdayaan_masyarakat.php to point to ../../handlers/form_sktm.php

--- dist/pages/forms/perlindungan_sosial,_pembangunan,_dan_pemberdayaan_masyarakat.php
+++ dist/pages/forms/perlindungan_sosial,_pembangunan,_dan_pemberdayaan_masyarakat.php
@@ -218,7 +218,7 @@ include "../../config/session.php";
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <form action="" method="post">
+              <form action="../../handlers/form_sktm.php" method="post">
                 <div class="row">
                   <div class="form-group mb-3">
                     <label class="mb-2">Jumlah SKTM yang Dikeluarkan</label>
