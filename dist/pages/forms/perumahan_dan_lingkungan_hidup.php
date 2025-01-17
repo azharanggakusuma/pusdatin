a65672959f5a453a981657b95b3cf6b613843b3a
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-01-17 10:25:59 +0700

    Update form action in perumahan_dan_lingkungan_hidup.php to point to form_embung.php handler.

--- dist/pages/forms/perumahan_dan_lingkungan_hidup.php
+++ dist/pages/forms/perumahan_dan_lingkungan_hidup.php
@@ -905,7 +905,7 @@ include "../../config/session.php";
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <form action="../../handlers/form_ketersediaan_penetapan_batas.php" method="post">
+              <form action="../../handlers/form_embung.php" method="post">
                 <div class="row">
                   <div class="form-group mb-3">
                     <label class="mb-2">Jumlah Embung</label>
