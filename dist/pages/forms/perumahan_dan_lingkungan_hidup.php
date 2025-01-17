a903e4186c759fe2088eda31cdc2f654b5008515
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-01-17 09:39:18 +0700

    Update form action in perumahan_dan_lingkungan_hidup.php to point to form_keberadaan_sungai.php handler.

--- dist/pages/forms/perumahan_dan_lingkungan_hidup.php
+++ dist/pages/forms/perumahan_dan_lingkungan_hidup.php
@@ -541,7 +541,7 @@ include "../../config/session.php";
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <form action="../../handlers/form_ketersediaan_penetapan_batas.php" method="post">
+              <form action="../../handlers/form_keberadaan_sungai.php" method="post">
                 <div class="row">
                   <div class="form-group mb-3">
                     <label class="mb-2">Keberadaan Sungai Yang Melintasi</label>
